0c9e30bcc6a48f595ad3b954688bcc7568f7d6de
Jonathan <xteazz06@gmail.com>
2018-09-25 23:32:39 +0200

    Cluster ok

--- js/GoogleMap.js
+++ js/GoogleMap.js
@@ -4,7 +4,9 @@ var GoogleMap = {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 45.750000, lng: 4.850000},
       zoom: 13});
+
     ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=65af477a79c3c0eca50cdd4a95bac21e8e17095b", function(reponse) {
+      var markers = [];
       var stations = JSON.parse(reponse);
       stations.forEach(function (station) {
         var marker = new google.maps.Marker({
@@ -12,10 +14,9 @@ var GoogleMap = {
           title: station.name,
           map: map
         });
-        var markers = [];
         markers.push(marker);
-        var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
       });
+      var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
     });
   },
 
