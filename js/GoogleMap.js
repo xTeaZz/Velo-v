b9e20dc2442a301a5e68bf505c5c75ca691e1ef3
Jonathan <xteazz06@gmail.com>
2018-10-04 11:01:05 +0200

    commencement compteur + session sotrage + commentaires

--- js/GoogleMap.js
+++ js/GoogleMap.js
@@ -1,10 +1,12 @@
 var GoogleMap = {
 
+  //Initialise la googleMap
   initBikeMap: function() {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 45.750000, lng: 4.850000},
       zoom: 13});
 
+    //Recupere les informations de l'api jcdecaux et cree les marqueurs
     ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=65af477a79c3c0eca50cdd4a95bac21e8e17095b", function(reponse) {
       var markers = [];
       var stations = JSON.parse(reponse);
@@ -15,6 +17,7 @@ var GoogleMap = {
           number: station.number,
           map: map
         });
+        //Remplie les informations lorsqu'on clique sur un marqueur
         marker.addListener('click', function() {
         ajaxGet("https://api.jcdecaux.com/vls/v1/stations/"+this.number+"?contract=Lyon&apiKey=65af477a79c3c0eca50cdd4a95bac21e8e17095b", function(reponse){
           var station = JSON.parse(reponse);
@@ -26,6 +29,7 @@ var GoogleMap = {
         });
         markers.push(marker);
       });
+      //Cluster
       var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
     });
   },
--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+//Tableau des textes et images
 picArray = ["images/velov1.jpg", "images/velov2.jpg", "images/velov3.jpg"];
 textArray = ["test1","test2", "test3"];
+
+//initialisation des objets
 var map = Object.create(GoogleMap);
 var slideShow = Object.create(Diaporama);
+
+//initialisation du diaporama avec les textes et images en parametre
 slideShow.init(picArray, textArray);
+
+//Ecoute l'utilisation des fleches directionnel du clavier pour changer le diaporama
 document.addEventListener("keydown", function(e) {
   if (e.keyCode == '37') {
     slideShow.transition(-1)
@@ -11,22 +18,76 @@ document.addEventListener("keydown", function(e) {
   }
 });
 
+//Action Diaporama lorsqu'on appuie sur le bouton gauche
 var buttonleft = document.getElementById("buttonleft");
-var buttonright = document.getElementById("buttonright");
-
 buttonleft.addEventListener("click", function() {
   slideShow.transition(-1)
 });
+
+//Action Diaporama lorsqu'on appuie sur le bouton droite
+var buttonright = document.getElementById("buttonright");
 buttonright.addEventListener("click", function() {
   slideShow.transition(1)
 });
 
+//Affiche le panneau de signature et de validation avec le bouton reservation
 var reservation = document.getElementById("reservationButton");
-
 reservation.addEventListener("click", function() {
   document.getElementById("signature").style.display = "block";
 });
 
+//Annule la reservation
+var annuler = document.getElementById("annuler");
+annuler.addEventListener("click", function() {
+  document.getElementById("signature").style.display = "none";
+});
+
+//Action lorsqu'on appuie sur valider
+var valider = document.getElementById("valider");
+valider.addEventListener("click", function() {
+  var name = document.getElementById("stationName");
+  var monTexte = name.innerText || name.textContent;
+  document.getElementById("reservationText").textContent = "Vélo réservé à la station " + monTexte + " par";
+  document.getElementById("decompte").textContent = "Temps restant"
+});
+
+//Sauvegarde en sessionStorage du prenom
+var prenomSave = document.getElementById("prenom");
+if (sessionStorage.getItem("autosave")) {
+  prenomSave.value = sessionStorage.getItem("autosave");
+}
+prenomSave.addEventListener("change", function() {
+  sessionStorage.setItem("autosave", prenomSave.value);
+});
+
+//Sauvegarde en sessionStorage du nom
+var nomSave = document.getElementById("nom");
+if (sessionStorage.getItem("autosave")) {
+  nomSave.value = sessionStorage.getItem("autosave");
+}
+nomSave.addEventListener("change", function() {
+  sessionStorage.setItem("autosave", nomSave.value);
+});
+
+var compteurElt = document.getElementById("compteur");
+// Diminue le compteur jusqu'à 0
+function diminuerCompteur() {
+    // Conversion en nombre du texte du compteur
+    var compteur = Number(compteurElt.textContent);
+    if (compteur > 1) {
+        compteurElt.textContent = compteur - 1;
+    } else {
+        // Annule l'exécution répétée
+        clearInterval(intervalId);
+        // Modifie le titre de la page
+        var titre = document.getElementById("titre");
+        titre.textContent = "BOUM !!!";
+    }
+}
+// Appelle la fonction diminuerCompteur toutes les secondes (1000 millisecondes)
+var intervalId = setInterval(diminuerCompteur, 1000);
+
+//initialisation de la googleMap
 function initMap() {
   GoogleMap.initBikeMap();
 }
