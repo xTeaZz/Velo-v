0c8d0b89366c30f74d7563bac8c53c8fbd31e6d2
Unknown <xteazz06@gmail.com>
2018-10-08 13:59:04 +0200

    Continuation localstorage reservation

--- js/main.js
+++ js/main.js
@@ -50,28 +50,34 @@ valider.addEventListener("click", function() {
   document.getElementById("signature").style.display = "none";
   document.getElementById("reservationText").textContent = "Vélo réservé à la station " + monTexte + " par";
   document.getElementById("decompte").textContent = "Temps restant"
+  document.getElementById("compteur").style.visibility = "visible";
   var intervalId = setInterval(diminuerCompteur, 1000);
 });
 
 //Sauvegarde en sessionStorage du prenom
 var prenomSave = document.getElementById("prenom");
-if (sessionStorage.getItem("prenom")) {
-  prenomSave.value = sessionStorage.getItem("prenom");
+if (localStorage.getItem("prenom")) {
+  prenomSave.value = localStorage.getItem("prenom");
 }
 prenomSave.addEventListener("change", function() {
-  sessionStorage.setItem("prenom", prenomSave.value);
+  localStorage.setItem("prenom", prenomSave.value);
 });
 
 //Sauvegarde en sessionStorage du nom
 var nomSave = document.getElementById("nom");
-if (sessionStorage.getItem("nom")) {
-  nomSave.value = sessionStorage.getItem("nom");
+if (localStorage.getItem("nom")) {
+  nomSave.value = localStorage.getItem("nom");
 }
 nomSave.addEventListener("change", function() {
-  sessionStorage.setItem("nom", nomSave.value);
+  localStorage.setItem("nom", nomSave.value);
 });
 
 var compteurElt = document.getElementById("compteur");
+if (sessionStorage.getItem("compteur")) {
+  nomSave.value = sessionStorage.getItem("compteur");
+  document.getElementById("compteur").style.visibility = "visible";
+  var intervalId = setInterval(diminuerCompteur, compteur);
+}
 // Diminue le compteur jusqu'à 0
 function diminuerCompteur() {
     // Conversion en nombre du texte du compteur
@@ -82,8 +88,8 @@ function diminuerCompteur() {
         // Annule l'exécution répétée
         clearInterval(intervalId);
         // Modifie le titre de la page
-        var titre = document.getElementById("titre");
-        titre.textContent = "BOUM !!!";
+        var titre = document.getElementById("compteur");
+        titre.textContent = "La réservation à expirer";
     }
 }
 // Appelle la fonction diminuerCompteur toutes les secondes (1000 millisecondes)
